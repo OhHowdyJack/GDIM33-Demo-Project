4987f725ff650daa529557f7c2a93d14323dfa89
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2025-03-26 16:59:16 -0700

    npc icon faces camera, activates w dist

---
+++ Herne 33/Assets/Scripts/DialogueNode.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct PlayerReply
+{
+    public string line;
+    public DialogueNode nextNode;
+}
+
+
+[CreateAssetMenu(fileName = "DialogueLine", menuName = "ScriptableObjects/DialogueLine", order = 1)]
+public class DialogueNode : ScriptableObject
+{
+    [Tooltip("The lines of dialogue the NPC says in this node.")]
+    public string[] Lines;
+
+    [Tooltip("The dialogue options for the player's response, and what node each response leads to.")]
+    public PlayerReply[] ReplyOptions;
+}
